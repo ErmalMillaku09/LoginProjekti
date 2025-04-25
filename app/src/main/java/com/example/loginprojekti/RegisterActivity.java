23af6901549e85223ccb1ae47299a3bf0a78a6c3
ErmalMillaku09 <ermal.millaku@student.uni-pr.edu>
2025-04-25 12:20:52 +0200

    Added OTP flow

--- app/src/main/java/com/example/loginprojekti/RegisterActivity.java
+++ app/src/main/java/com/example/loginprojekti/RegisterActivity.java
@@ -24,7 +24,6 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
